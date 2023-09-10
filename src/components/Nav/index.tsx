528cbae7b4d3b5f615139b4a7290cc0693be69ad
Gustavonn07 <gustavonepnog@gmail.com>
2023-09-10 13:09:18 -0300

    add arquitetura base 01

---
+++ src/components/Nav/index.tsx
@@ -0,0 +1,18 @@
+import { styled } from 'styled-components';
+
+const Nav = () => {
+
+    return (
+        <>
+            <Nav__Container>
+                
+            </Nav__Container>
+        </>
+    )
+}
+
+const Nav__Container = styled.main`
+    
+`
+
+export default Nav;
